8bcdf451079194e87e2b1235c8d4a0b3683125e6
Pikachuxxxx <phani.s2909@icloud.com>
2020-06-14 15:06:03 +0530

    Added library and initialised rect scaling project

---
+++ Raybli-Shapes-RectScaling/Raybli-Shapes-RectScaling/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Raybli-Shapes-RectScaling
+//
+//  Created by phani srikar on 14/06/20.
+//  Copyright © 2020 phani srikar. All rights reserved.
+//
+
+#include <stdio.h>
+#include <raylib.h>
+
+int main() {
+    
+    printf("Hello, World!\n");
+    return 0;
+}
