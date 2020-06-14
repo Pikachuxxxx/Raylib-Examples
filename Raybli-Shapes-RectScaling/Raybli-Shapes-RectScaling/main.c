eedc9bee8b657d32d8e226825ef745e3f1afa44b
Pikachuxxxx <phani.s2909@icloud.com>
2020-06-14 16:11:58 +0530

    Finished scaling rectangle

--- Raybli-Shapes-RectScaling/Raybli-Shapes-RectScaling/main.c
+++ Raybli-Shapes-RectScaling/Raybli-Shapes-RectScaling/main.c
@@ -9,13 +9,62 @@
 #include <stdio.h>
 #include <raylib.h>
 
+#define MOUSE_SCALE_MARK_SIZE   12
+
 int main() {
     const int screenWidth = 800, screenHeight = 700;
     InitWindow(screenWidth, screenHeight, "Raylib [Shapes] : Rectangle Scaling");
     SetTargetFPS(65);
     
+    Rectangle rec = { 100, 100, 200, 80 };
+    Vector2 mousePosition = { 0 };
+
+    bool mouseScaleReady = false;
+    bool mouseScaleMode = false;
+
+    
     while (!WindowShouldClose()) {
+
+        mousePosition = GetMousePosition();
+
+        if (CheckCollisionPointRec(mousePosition, rec))// && CheckCollisionPointRec(mousePosition, (Rectangle){ rec.x + rec.width - MOUSE_SCALE_MARK_SIZE, rec.y + rec.height - MOUSE_SCALE_MARK_SIZE, MOUSE_SCALE_MARK_SIZE, MOUSE_SCALE_MARK_SIZE }))
+               {
+                   mouseScaleReady = true;
+                   if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) mouseScaleMode = true;
+               }
+               else mouseScaleReady = false;
         
+        if (mouseScaleMode)
+        {
+            mouseScaleReady = true;
+
+            rec.width = (mousePosition.x - rec.x);
+            rec.height = (mousePosition.y - rec.y);
+
+//            if (rec.width < MOUSE_SCALE_MARK_SIZE) rec.width = MOUSE_SCALE_MARK_SIZE;
+//            if (rec.height < MOUSE_SCALE_MARK_SIZE) rec.height = MOUSE_SCALE_MARK_SIZE;
+
+            if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) mouseScaleMode = false;
+        }
+        
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+//        Rectangle testRect = (Rectangle){ rec.x + rec.width - MOUSE_SCALE_MARK_SIZE, rec.y + rec.height - MOUSE_SCALE_MARK_SIZE, MOUSE_SCALE_MARK_SIZE, MOUSE_SCALE_MARK_SIZE };
+//        DrawRectangleRec(testRect, RED);
+
+        
+        DrawRectangleRec(rec, Fade(GREEN, 0.5f));
+               
+        
+        if (mouseScaleReady)
+        {
+            DrawRectangleLinesEx(rec, 1, RED);
+            DrawTriangle((Vector2){ rec.x + rec.width - MOUSE_SCALE_MARK_SIZE, rec.y + rec.height },
+                         (Vector2){ rec.x + rec.width, rec.y + rec.height },
+                         (Vector2){ rec.x + rec.width, rec.y + rec.height - MOUSE_SCALE_MARK_SIZE }, RED);
+        }
+        EndDrawing();
     }
     
     CloseWindow();
